7600e9a7802351f9932635c6bd86c9188ee076e2
Ryan Quinn <quinn.ryanx@gmail.com>
2017-07-07 11:25:20 +0000

    Added write functions

--- IPBus_Test/include/ipbus_common.hpp
+++ IPBus_Test/include/ipbus_common.hpp
@@ -2,7 +2,16 @@
 #define IPBUS_COMMON_HPP
 
 #include "uhal/uhal.hpp"
+#include <vector>
+#include <string>
 
+
+/*** GENERAL ***/
+void die(const char*);
+// prints the string and exits the program
+
+
+/*** READ ***/
 uint32_t get_word(uhal::HwInterface*, const char*);
 // reads a single word from a register
 
@@ -14,5 +23,20 @@ std::vector<uint32_t> get_word_multi_nodes(uhal::HwInterface*, std::vector<std::
 
 std::vector<std::vector<uint32_t>> get_nwords_multi_nodes(uhal::HwInterface*, std::vector<std::string>, std::vector<int>);
 // reads multiple words from multiple registers
+
+
+/*** WRITE ***/
+void send_word(uhal::HwInterface*, const char*, uint32_t);
+// writes a single word to a register
+
+void send_nwords(uhal::HwInterface*, const char*, std::vector<uint32_t>);
+// writes N words to a register
+
+void send_word_multi_nodes(uhal::HwInterface*, std::vector<std::string>, std::vector<uint32_t>);
+// writes a single word to multiple registers
+
+void send_nwords_multi_nodes(uhal::HwInterface*, std::vector<std::string>, std::vector<std::vector<uint32_t>>);
+// writes multiple words to multiple registers
+
 
 #endif
--- IPBus_Test/src/common/ipbus_common.cpp
+++ IPBus_Test/src/common/ipbus_common.cpp
@@ -8,6 +8,18 @@
 using namespace uhal;
 
 
+
+/*** GENERAL ***/
+void die(const char *s) {
+/* --- prints the string and kills the program --- */
+
+	std::cout << s << std::endl;
+	exit(1);
+}
+
+
+
+/*** READ ***/
 uint32_t get_word(HwInterface *hw, const char *name) {
 /* --- reads a single word from a node ---
  *
@@ -65,10 +77,8 @@ std::vector<std::vector<uint32_t>> get_nwords_multi_nodes(HwInterface *hw, std::
  * - num_words: vector containing the number of words to read from each node
  */
 
-	if(names.size() != num_words.size()) {
-		std::cout << "names and num_words do not have the same size" << std::endl;
-		exit(0);
-	}
+	if(names.size() != num_words.size())
+		die("names and num_words do not have the same size");
 
 	std::vector<std::vector<uint32_t>> words;
 
@@ -80,4 +90,67 @@ std::vector<std::vector<uint32_t>> get_nwords_multi_nodes(HwInterface *hw, std::
 	hw->dispatch();
 
 	return words;
+}
+
+
+
+/*** WRITE ***/
+void send_word(HwInterface *hw, const char *name, uint32_t word) {
+/* --- writes a single word to a node ---
+ *
+ * - hw: the hardware interface (must be passed by reference)
+ * - name: the name of the node to access
+ * - word: the value to write
+ */
+
+	hw->getNode(name).write(word);
+	hw->dispatch();
+	return;
+}
+
+
+void send_nwords(HwInterface *hw, const char *name, std::vector<uint32_t> words) {
+/* --- writes multiple words to a single node ---
+ *
+ * - hw: the hardware interface (must be passed by reference)
+ * - name: the name of the node to access
+ * - words: vector containing the words to write to the node
+ */
+
+	hw->getNode(name).writeBlock(words);
+	hw->dispatch();
+	return;
+}
+
+
+void send_word_multi_nodes(HwInterface *hw, std::vector<std::string> names, std::vector<uint32_t> words) {
+/* --- writes a single word to multiple nodes ---
+ *
+ * - hw: the hardware interface (must be passed by reference)
+ * - names: vector containing the names of the nodes to be written to (as strings)
+ * - words: vector containing the words to write to each node
+ */
+
+	for(unsigned int i = 0; i < words.size(); i++)
+		hw->getNode(names[i].c_str()).write(words[i]);
+	hw->dispatch();
+	return;
+}
+
+
+void send_nwords_multi_nodes(HwInterface *hw, std::vector<std::string> names, std::vector<std::vector<uint32_t>> words) {
+/* --- writes multiple words to multiple nodes ---
+ *
+ * - hw: the hardware interface (must be passed by reference)
+ * - names: vector containing the names of the nodes to be write to (as strings)
+ * - words: vector containing the blocks of words to write to each node
+ */
+
+	if(names.size() != words.size())
+		die("names and words do not have the same size");
+
+	for(unsigned int i = 0; i < names.size(); i++)
+		hw->getNode(names[i].c_str()).writeBlock(words[i]);
+	hw->dispatch();
+	return;
 }
