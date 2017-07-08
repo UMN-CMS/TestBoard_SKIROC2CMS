298868777a54c1348eb9047ae96f4b8e39b0f2d1
Ryan Quinn <quinn.ryanx@gmail.com>
2017-07-08 16:18:16 +0000

    Working on hexaboard_to_fifo functions. Added a check_block_ready function

--- IPBus_Test/src/common/pedestal_daq.cxx
+++ IPBus_Test/src/common/pedestal_daq.cxx
@@ -9,10 +9,20 @@ const int RAWSIZE = 30787;
 
 using namespace uhal;
 
+bool check_block_ready(HwInterface *hw, std::vector<std::string> active_fifos);
+std::vector<std::string> hexaboard_to_fifo(std::vector<int>);
+std::vector<std::string> hexaboard_to_fifo(int);
+
 int main(int argc, char *argv[]){
 
 	// argument processing
-	int maxevents = 10;
+	// if(argc < 3)
+		// die("usage: ./bin/pedestal daq [RUN NUMBER] [EVENTS]");
+	int RUN_NUMBER = argc < 3 ? 0 : atoi(argv[1]);
+	int MAXEVENTS = argc < 3 ? 1 : atoi(argv[2]);
+
+	std::cout << "Run number: " << RUN_NUMBER << std::endl;
+	std::cout << "Events: " << MAXEVENTS << std::endl;
 
 
 	// initialization
@@ -22,32 +32,31 @@ int main(int argc, char *argv[]){
 	// set up connections
 	ConnectionManager manager("file://xml/connections.xml");
 	std::vector<std::string> ids = manager.getDevices();
-
 	std::vector<HwInterface> rdouts;
-	std::vector<std::string>::const_iterator id_iter, id_end;// if only we could use C++ 11 ... for(auto id : ids)
-	for(id_end=ids.end(), id_iter=ids.begin(); id_iter != id_end; ++id_iter) {
-		std::string id = *id_iter;
-
-		rdouts.push_back(manager.getDevice(id));
-	}
+	for(unsigned int i = 0; i < ids.size(); i++)
+		rdouts.push_back(manager.getDevice(ids[i]));
 
 
 	// loop over events
-	for(int event = 0; event < maxevents; event++) {
+	for(int event = 0; event < MAXEVENTS; event++) {
 		// loop over readout boards
 		std::vector<HwInterface>::iterator hw_iter, hw_end;// C++ 11 ... for(auto& rdout : rdouts)
 		for(hw_end=rdouts.end(), hw_iter=rdouts.begin(); hw_iter != hw_end; ++hw_iter) {
 			HwInterface& rdout = *hw_iter;
 
-			std::vector<std::string> fifo_list = rdout->getNodes("FIFO.*");
-
-			// loop over hexaboards (fifos for now)
-			for(unsigned int i = 0; i < fifo_list.size(); i++) {
-				std::cout << fifo_list[i] << std::endl;
-			}
+			std::vector<std::string> fifo_list = rdout.getNodes("FIFO.*");
+			std::vector<std::string> active_fifos = fifo_list;// all fifos(for now)
 
 			// trigger, etc...
-			// read raw
+			while(check_block_ready(&rdout, active_fifos)) {
+				std::cout << "Block not ready!" << std::endl;
+				std::cout << "Continuing anyway :)" << std::endl;
+				break;
+			}
+
+			// read data from each hexaboard
+			std::vector<uint32_t> data = get_word_multi_nodes(&rdout, fifo_list);
+
 			// decode raw
 			// verify data
 			// format channels
@@ -59,4 +68,28 @@ int main(int argc, char *argv[]){
 
 	// closing actions
 	return 0;
+}
+
+
+bool check_block_ready(HwInterface *hw, std::vector<std::string> active_fifos) {
+	uint32_t block_ready = get_block_ready(hw);
+
+	for(unsigned int i = 0; i < active_fifos.size(); i++) {
+		int fifo_num = atoi(active_fifos[i].substr(5).c_str());
+
+		if(block_ready & (1 << fifo_num))
+			return false;
+	}
+
+	return true;
+}
+
+
+std::vector<string> hexaboard_to_fifo(int) {
+
+}
+
+
+std::vector<string> hexaboard_to_fifo(std::vector<int>) {
+
 }
